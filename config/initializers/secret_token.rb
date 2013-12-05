59e6925f9f01555b00f6503ca8ec5e0a24219307
Meredith Bryan <merbryan@gmail.com>
2013-12-05 08:10:17 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoo::Application.config.secret_key_base = 'd36552a2a589670f6f5c5aa1bdb373d34f5a9a4c7613f09a2410996f4e3180f3c823ac3bb9626e7b21de282d9a09953330127f6694545d50ae40a9bc3a49f0c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zoo::Application.config.session_store :cookie_store, key: '_zoo_session'
